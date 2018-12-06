c29b3cfbf63752b3489ec633413aad3e354a8435
Tom McLaughlan <tmclaughlan@pushtechnology.com>
2018-12-06 13:06:09 +0000

    Initial work on the main screen component

--- quiz-app/src/app/screen/screen.component.ts
+++ quiz-app/src/app/screen/screen.component.ts
@@ -1,10 +1,48 @@
 import { Component } from "@angular/core";
+import { WebSocketSubject } from 'rxjs/internal/observable/dom/WebSocketSubject';
+import { CommEvent } from '../../../../server/src/main';
+import { WebsocketService } from '../websocket.service';
 
+export interface Question {
+    type : string;
+    data : any;
+}
 
 @Component({
     selector : 'screen',
     templateUrl : './screen.component.html'
 })
 export class ScreenComponent {
+    public question : Question;
+    private socket$ : WebSocketSubject<CommEvent>;
+
+    constructor(public websocket : WebsocketService) {
+        console.log('hello');
+        this.socket$ = websocket.openSocket();
+        this.socket$.subscribe((msg : CommEvent) => {
+            if (msg.action === 'question') {
+                this.question = msg.question;
+            }
+        });
+
+        this.question = {
+            type : 'answersmash',
+            data : {
+                image : 'gimli.jpeg',
+                question : 'Which band had their only UK number 1 with "Rollin\'"?'
+            }
+        };
+    }
+
+    public getType() {
+        if (this.question) {
+            return this.question.type;
+        }
+        return false;
+    }
+
+    public getQuestion() {
+        return this.question;
+    }
     
 }
